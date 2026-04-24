df7162086649ebb9273f242dc3ab284269c17833
Amrit Jha <amritkumarjha7777@gmail.com>
2026-04-24 12:04:43 +0530

    Implement MultipleFilters component with filtering logic
    Given a list of products and a list of filters, create a multi-filters buttons bar with a list of filters. If any of the filter is clicked, the list of products should change and reflect the appropriate products.

---
+++ multiple-filters.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { items as defaultItems } from './items';
+import './style.css';
+
+export default function MultipleFilters() {
+  const [items, setItems] = useState(defaultItems);
+  const [selectedFilters, setSelectedFilters] = useState<any>([]);
+
+  let filters = ['Bags', 'Watches', 'Sports', 'Sunglasses'];
+
+  const handleFilterClick = (el) => {
+    if (selectedFilters.includes(el))
+      setSelectedFilters((prev) => prev.filter((item) => item !== el));
+    else setSelectedFilters((prev) => [...prev, el]);
+  };
+
+  return (
+    <div>
+      <h2 style={{ textAlign: 'center' }}>Algochurn Filters</h2>
+      <div className="buttons-container">
+        {filters.map((el, idx) => (
+          <button
+            className={`button`}
+            key={`filters-${idx}`}
+            onClick={() => handleFilterClick(el)}
+          >
+            {el}
+          </button>
+        ))}
+      </div>
+      <div className="items-container">
+        {(selectedFilters.length === 0
+          ? items
+          : items.filter((item) => selectedFilters.includes(item.category))
+        ).map((item, idx) => (
+          <div key={`items-${idx}`} className="item">
+            <p>{item.name}</p>
+            <p className="category">{item.category}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
